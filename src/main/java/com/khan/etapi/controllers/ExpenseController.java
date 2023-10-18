6bba5584d2d23a4d90f4e581d33f0e41245d8251
imranahmedkhan@hotmail.co.uk <imranahmedkhan@hotmail.co.uk>
2023-10-18 23:03:10 +0100

    Added the http Status codes to REST end Points

--- src/main/java/com/khan/etapi/controllers/ExpenseController.java
+++ src/main/java/com/khan/etapi/controllers/ExpenseController.java
@@ -3,6 +3,7 @@ package com.khan.etapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.khan.etapi.entities.Expense;
@@ -30,7 +32,7 @@ public class ExpenseController {
 		
 		return expenseService.getExpenseById(id);
 	}
-
+	@ResponseStatus(value=HttpStatus.CREATED)//this will create a 201 code
 	@PostMapping("/expenses")
 	public Expense saveExpenseDetails(@RequestBody Expense expense) {
 		return expenseService.saveExpenseDetails(expense);
@@ -41,7 +43,7 @@ public class ExpenseController {
 	return expenseService.updateExpenseDetails(id, expense);
 
 		}
-
+	@ResponseStatus(value=HttpStatus.NO_CONTENT)//this will create a 204 code
 	@DeleteMapping("/expenses")
 	public void deleteExpenseById(@RequestParam("id") Long id) {
 		expenseService.deleteExpenseById(id);
